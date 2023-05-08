416058642b645fdcd15477a32fcffcb0c295af05
Tonio Adams <avcx12@gmail.com>
2023-05-08 17:53:05 -0600

    Fixed project route

--- routes/projects.js
+++ routes/projects.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router =  express.Router();
 const { projects } = require('../data.json');
 
-router.get('/project/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     const projectNum = req.params.id;
     res.render('project', {});
 })
