2cd74ee643b0d767b612a3c2862efd76082e0c5c
Tonio Adams <avcx12@gmail.com>
2023-05-08 17:00:00 -0600

    Set up the basic routes & the static server

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+const mainRoutes = require('./routes/index');
 
 app.set('view engine', 'pug');
+app.use('/static', express.static('./public'));
+
+app.use(mainRoutes);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found!');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status);
+    res.render('', {error: err});
+});
 
 app.listen(3000, () => {
     console.log('App is listening on port 3000!');
-})
+});
